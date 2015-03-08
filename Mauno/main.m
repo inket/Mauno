49dc1eb82f5212af2339199175a8d28ce35a7e29
inket <injekter@gmail.com>
2015-03-09 00:20:36 +0900

    Initial commit

---
+++ Mauno/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mauno
+//
+//  Created by Mahdi Bchetnia on 11/08/2012.
+//  Copyright (c) 2012 Mahdi Bchetnia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
